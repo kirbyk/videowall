41d4d5e70739806b6cbae41a481e3cb2c83997a8
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2014-03-12 18:17:45 -0400

    Basic rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Videowall::Application.config.secret_key_base = '2ff7e586487c5b581fe010677d399968bd95a264fe763213a9dec4d16e51c7e16f78dc87b08b61cc1254379d7d25d764fb3990d732ed1f06e33f0fbbed6dd37a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Videowall::Application.config.session_store :cookie_store, key: '_videowall_session'
